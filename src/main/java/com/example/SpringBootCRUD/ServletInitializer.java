8ff1e15e46674c37d8c31359c7688e3c135693d6
FinallRareStaff <Jeymester@gmail.com>
2022-04-21 13:48:18 +0300

    Added info to application.properties

--- src/main/java/com/example/SpringBootCRUD/ServletInitializer.java
+++ src/main/java/com/example/SpringBootCRUD/ServletInitializer.java
@@ -2,12 +2,27 @@ package com.example.SpringBootCRUD;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(SpringBootCrudApplication.class);
+	}
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		super.onStartup(servletContext);
+		registerHiddenFieldFilter(servletContext);
+	}
+
+	private void registerHiddenFieldFilter(ServletContext aContext) {
+		aContext.addFilter("hiddenHttpMethodFilter",
+				new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
 	}
 
 }
